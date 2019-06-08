1f38243db1942556bedaefa75a13c5174f988a31
Watchful1 <watchful@watchful.gr>
2019-06-07 23:14:16 -0700

    Clean up arg parsing

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ import signal
 import time
 import traceback
 import discord_logging
+import argparse
 
 log = discord_logging.init_logging()
 
@@ -18,73 +19,44 @@ import notifications
 import utils
 
 
-remind_me_bot = None
+database = None
 
 
 def signal_handler(signal, frame):
 	log.info("Handling interrupt")
-	remind_me_bot.close()
+	database.close()
 	sys.exit(0)
 
 
 signal.signal(signal.SIGINT, signal_handler)
 
 
-class RemindMeBot:
-	def __init__(self):
-		self.once = False
-		self.debug_db = False
-		self.clone_db = False
-		self.no_post = False
-		self.no_backup = False
-		self.user = None
-		if len(sys.argv) >= 2:
-			self.user = sys.argv[1]
-			for arg in sys.argv:
-				if arg == 'once':
-					self.once = True
-				elif arg == 'debug_db':
-					self.debug_db = True
-				elif arg == 'clone_db':
-					self.clone_db = True
-				elif arg == 'no_post':
-					self.no_post = True
-				elif arg == 'no_backup':
-					self.no_backup = True
-				elif arg == 'debug':
-					discord_logging.set_level(logging.DEBUG)
-		else:
-			log.error("No user specified, aborting")
-			raise ValueError
-
-		discord_logging.init_discord_logging(self.user)
-
-		self.reddit = reddit_class.Reddit(self.user, self.no_post)
-
-		self.database = database_class.Database(debug=self.debug_db, clone=self.clone_db)
-
-	def close(self):
-		self.database.close()
-
-	def process_messages(self):
-		messages.process_messages(self.reddit, self.database)
-
-	def process_comments(self):
-		comments.process_comments(self.reddit, self.database)
-
-	def update_comments(self):
-		comments.update_comments(self.reddit, self.database)
-
-	def send_notifications(self):
-		notifications.send_reminders(self.reddit, self.database)
-		notifications.send_cakeday_notifications(self.reddit, self.database)
-
-	def backup_database(self):
-		self.database.backup()
-
-
 if __name__ == "__main__":
-	remind_me_bot = RemindMeBot()
+	parser = argparse.ArgumentParser(description="Reddit RemindMe bot")
+	parser.add_argument("user", help="The reddit user account to use")
+	parser.add_argument("--once", help="Only run the loop once", action='store_const', const=True, default=False)
+	parser.add_argument("--debug_db", help="Use the debug database", action='store_const', const=True, default=False)
+	parser.add_argument(
+		"--clone_db", help="Copy the primary database to the debug on", action='store_const', const=True, default=False)
+	parser.add_argument(
+		"--no_post", help="Print out reddit actions instead of posting to reddit", action='store_const', const=True,
+		default=False)
+	parser.add_argument(
+		"--no_backup", help="Don't backup the database", action='store_const', const=True, default=False)
+	parser.add_argument(
+		"--reset_comment", help="Reset the last comment read timestamp", action='store_const', const=True,
+		default=False)
+	parser.add_argument("--debug", help="Set the log level to debug", action='store_const', const=True, default=False)
+	args = parser.parse_args()
+
+	if args.debug:
+		discord_logging.set_level(logging.DEBUG)
+
+	discord_logging.init_discord_logging(args.user)
+	reddit = reddit_class.Reddit(args.user, args.no_post)
+	database = database_class.Database(debug=args.debug_db, clone=args.clone_db)
+	if args.reset_comment:
+		database.save_keystore("remindme_comment", utils.get_datetime_string(utils.datetime_now()))
 
 	last_backup = None
 	last_comments = None
@@ -93,34 +65,35 @@ if __name__ == "__main__":
 		log.debug("Starting run")
 
 		try:
-			remind_me_bot.process_messages()
+			messages.process_messages(reddit, database)
 		except Exception as err:
 			log.warning(f"Error processing messages: {err}")
 			log.warning(traceback.format_exc())
 
 		try:
-			remind_me_bot.process_comments()
+			comments.process_comments(reddit, database)
 		except Exception as err:
 			log.warning(f"Error processing comments: {err}")
 			log.warning(traceback.format_exc())
 
 		try:
-			remind_me_bot.send_notifications()
+			notifications.send_reminders(reddit, database)
+			notifications.send_cakeday_notifications(reddit, database)
 		except Exception as err:
 			log.warning(f"Error sending notifications: {err}")
 			log.warning(traceback.format_exc())
 
 		if utils.time_offset(last_comments, minutes=30):
 			try:
-				remind_me_bot.update_comments()
+				comments.update_comments(reddit, database)
 				last_comments = utils.datetime_now()
 			except Exception as err:
 				log.warning(f"Error updating comments: {err}")
 				log.warning(traceback.format_exc())
 
-		if not remind_me_bot.no_backup and utils.time_offset(last_backup, hours=24):
+		if not args.no_backup and utils.time_offset(last_backup, hours=24):
 			try:
-				remind_me_bot.backup_database()
+				database.backup()
 				last_backup = utils.datetime_now()
 			except Exception as err:
 				log.warning(f"Error backing up database: {err}")
@@ -128,6 +101,6 @@ if __name__ == "__main__":
 
 		log.debug("Run complete after: %d", int(time.perf_counter() - startTime))
 
-		if remind_me_bot.once:
+		if args.once:
 			break
 		time.sleep(static.LOOP_TIME)
